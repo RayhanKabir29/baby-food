c674504d619373a3889ecfe3af43799b42f85371
RayhanKabir29 <rayhan.kabir29@gmail.com>
2021-11-19 03:11:02 +0600

    star rating

--- src/Pages/AddReview/AddReview.js
+++ src/Pages/AddReview/AddReview.js
@@ -2,9 +2,14 @@ import axios from 'axios';
 import React from 'react';
 import { useForm } from "react-hook-form";
 import './AddReview.css';
+import ReactStars from "react-rating-stars-component";
 
 
 const AddReview = () => {
+    const ratingChanged = (newRating) => {
+        console.log(newRating);
+      };
+    
     const { register, handleSubmit, reset } = useForm();
     const onSubmit = data => {
         console.log(data);
@@ -21,6 +26,12 @@ const AddReview = () => {
         <div className="add-review">
 
         <form onSubmit={handleSubmit(onSubmit)}>
+        <h5>User Rating: <ReactStars
+                    count={5}
+                    onChange={ratingChanged}
+                    size={24}
+                    activeColor="#ffd700"
+                />,</h5>
          <input {...register("name", { required: true})}  placeholder="Name of Reviewer"/>
          <textarea {...register("review", { required: true})}  placeholder="Review Desc"/>
          <input type="submit" />
